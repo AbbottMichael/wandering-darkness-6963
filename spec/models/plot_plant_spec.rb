12e5a9fdf20cfa639ae3f33a80583ff9e6166ef8
Michael Abbott <michael@mjabbottdesign.com>
2021-08-06 09:13:36 -0700

    Create ::find_plot_plant model test

--- spec/models/plot_plant_spec.rb
+++ spec/models/plot_plant_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe PlotPlant do
     it { should belong_to(:plot) }
     it { should belong_to(:plant) }
   end
+
+  describe 'class methods' do
+    before :each do
+      @garden1 = Garden.create!(name: 'Garden1', organic: true)
+      @plot1 = @garden1.plots.create!(number: 10, size: 'Large', direction: 'East')
+      @plant1 = Plant.create!(name: 'Plant1', description: 'Description1', days_to_harvest: 50)
+      @plant2 = Plant.create!(name: 'Plant2', description: 'Description2', days_to_harvest: 200)
+      @plot_plant1 = PlotPlant.create!(plant_id: @plant1.id, plot_id: @plot1.id)
+      @plot_plant2 = PlotPlant.create!(plant_id: @plant2.id, plot_id: @plot1.id)
+
+    end
+
+    it "returns a PlotPlant instance when given a plant_id and a plot_id" do
+      expect(PlotPlant.find_plot_plant(@plant1.id, @plot1.id)).to eq(@plot_plant1)
+    end
+  end
 end
