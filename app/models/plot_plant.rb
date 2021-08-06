9d56f4c1b9ad03c49930a6bdfa7e7fc406ef909f
Michael Abbott <michael@mjabbottdesign.com>
2021-08-06 09:13:59 -0700

    Add ::find_plot_plant

--- app/models/plot_plant.rb
+++ app/models/plot_plant.rb
@@ -1,4 +1,8 @@
 class PlotPlant < ApplicationRecord
   belongs_to :plot
   belongs_to :plant
+
+  def self.find_plot_plant(plant_id, plot_id)
+    where(plant_id: plant_id, plot_id: plot_id).take
+  end
 end
